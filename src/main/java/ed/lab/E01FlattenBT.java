6f7181ef20d3afebb1e8828135bec2675fd215e4
marcs <mwschaub@correo.url.edu.gt>
2025-03-25 07:48:49 -0600

    Commit con ejercicio 1 y tests pasados

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,28 @@
 package ed.lab;
 
 public class E01FlattenBT {
-
     public void flatten(TreeNode<Integer> root) {
+        if (root == null) return;
 
+        // Recorrer el árbol en preorden para facilitar el ordenamiento
+        TreeNode<Integer> current = root;
+        while (current != null) {
+            if (current.left != null) {
+                // Encontrar el anterior del nodo
+                TreeNode<Integer> anterior = current.left;
+                while (anterior.right != null) {
+                    anterior = anterior.right;
+                }
+
+                //decirle el anterior
+                anterior.right = current.right;
+
+                current.right = current.left; // mover el arbol
+                current.left = null;
+            }
+
+            // Avanza para enlistar
+            current = current.right;
+        }
     }
-
 }
